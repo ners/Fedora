a42208c22473cbd9c0ab44c1d558275f35623003
ners <50560955+ners@users.noreply.github.com>
2021-03-24 01:13:45 +0100

    Make missing directories in install script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 find home -type f | sed 's|^home/||' | while read f; do
 	repopath="home/$f"
 	homepath="$HOME/$f"
+	mkdir -p $(dirname $homepath)
 	cp "$repopath" "$homepath"
 done
